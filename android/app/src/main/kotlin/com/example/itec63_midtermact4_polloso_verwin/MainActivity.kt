53bd812ae51c03a684e649c571f9aa17b725f2bd
Verwin Polloso <verwinpolloso2@gmail.com>
2026-03-01 12:41:01 +0800

    bruh

---
+++ android/app/src/main/kotlin/com/example/itec63_midtermact4_polloso_verwin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.itec63_midtermact4_polloso_verwin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      home: MyMidtermApp(),
+    ));
+
+class MyMidtermApp extends StatelessWidget {
+  const MyMidtermApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Polloso Program',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'Emaro',
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 19, 146, 36),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/jeffrey.jpg',
+              height: 250,
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 0, 255, 76),
+                border: Border.all(color: const Color.fromARGB(255, 35, 155, 71), width: 2),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: const Text(
+                'Verwin Polloso\n' 
+                'BSIT 3-1\n'       
+                'Silang, Cavite\n' 
+                '0906-1234-980',    
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  fontFamily: 'Emaro',
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, 
+        backgroundColor: const Color.fromARGB(255, 47, 238, 57),
+        child: const Center(child: Icon(Icons.thumb_up)), 
+      ),
+    );
+  }
+}
